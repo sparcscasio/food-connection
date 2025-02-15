910910b3ac8cfb0611dbef81d362d66541779146
JSJ <hulk3127@naver.com>
2025-02-16 07:23:55 +0900

    Login COmmit 오류수정

--- App.tsx
+++ App.tsx
@@ -1,32 +1,44 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { StyleSheet } from "react-native";
-import { RecoilRoot } from "recoil";
+import { RecoilRoot, useRecoilState } from "recoil";
 import FeedDetailArea from "./component/search/FeedDetailArea";
 import FollowerPage from "./screen/FollowerPage";
 import FollowingPage from "./screen/FollowingPage";
 import HomeScreen from "./screen/HomeScreen";
+import LoginPage from "./screen/LoginPage";
+import SignupPage from "./screen/SignupPage";
 import UserPage from "./screen/UserPage";
+import { loginState } from "./utils/recoil/atoms/loginState";
 
 export type RootStackParamList = {
   Home: undefined;
   FeedDetail: { uid: string };
-  ProfileDetail: { uid: string };
   FollowingPage: undefined;
   FollowerPage: undefined;
+  UserPage: { uid: String };
+  Login: undefined;
+  Signup: undefined;
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 const App2: React.FC = () => {
+  const [isLoggedIn, setIsLoggedIn] = useRecoilState(loginState);
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName={isLoggedIn ? "Home" : "Login"}
+      >
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="FeedDetail" component={FeedDetailArea} />
         <Stack.Screen name="FollowingPage" component={FollowingPage} />
         <Stack.Screen name="FollowerPage" component={FollowerPage} />
-        <Stack.Screen name="ProfileDetail" component={UserPage} />
+
+        <Stack.Screen name="UserPage" component={UserPage} />
+        <Stack.Screen name="Login" component={LoginPage} />
+        <Stack.Screen name="Signup" component={SignupPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
